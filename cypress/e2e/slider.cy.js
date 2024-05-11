ee3359cdec5ad8da6a833301bae84a574755d0c3
Rafał Turakiewicz <rafal.turakiewicz@britenet.com.pl>
2024-05-11 12:57:06 +0200

    Add hotjar and tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,78 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Next Button Test', function () {
+  it('Checks if next button and prev button works correctly', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Title Description Test', function () {
+  it('Checks if each page contains description and title', function () {
+    cy.visit('http://localhost:3000');
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+describe('Title Description Test', function () {
+  it('Checks if each page contains description and title', function () {
+    cy.visit('http://localhost:3000');
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+describe('Iphone 6 Compability Test', function () {
+  it('Checks if each page contains description and title on Iphone 6', function () {
+    cy.visit('http://localhost:3000');
+    cy.viewport('iphone-6');
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+
+describe('Iphone 6 Button Test', function () {
+  it('Check if buttons exist on Iphone 6', function () {
+    cy.visit('http://localhost:3000');
+    cy.viewport('iphone-6');
+    cy.wait(2000);   
+    cy.get('.swiper-button-next').should('exist');
+    cy.get('.swiper-button-prev').should('exist');
+  });
+});
